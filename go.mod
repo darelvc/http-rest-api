fb131aa107041e0005359a80b2ffc518b37b898f
Oleg Bloshenkin <darelvc@gmail.com>
2019-08-18 23:03:54 +0300

    Add logger

--- go.mod
+++ go.mod
@@ -1,3 +1,6 @@
 module github.com/darelvc/http-rest-api
 
-require github.com/BurntSushi/toml v0.3.1
+require (
+	github.com/BurntSushi/toml v0.3.1
+	github.com/sirupsen/logrus v1.4.2
+)
--- internal/app/apiserver/apiserver.go
+++ internal/app/apiserver/apiserver.go
@@ -1,18 +1,42 @@
 package apiserver
 
+import (
+	"github.com/sirupsen/logrus"
+)
+
 // APIServer ...
 type APIServer struct {
 	config *Config
+	logger *logrus.Logger
 }
 
 // New ...
 func New(config *Config) *APIServer {
 	return &APIServer{
 		config: config,
+		logger: logrus.New(),
 	}
 }
 
 // Start ...
 func (s *APIServer) Start() error {
+	if err := s.ConfirureLogger(); err != nil {
+		return err
+	}
+
+	s.logger.Info("Starting API server ...")
+
+	return nil
+}
+
+// ConfirureLogger ...
+func (s *APIServer) ConfirureLogger() error {
+	level, err := logrus.ParseLevel(s.config.LogLevel)
+	if err != nil {
+		return err
+	}
+
+	s.logger.SetLevel(level)
+
 	return nil
 }
